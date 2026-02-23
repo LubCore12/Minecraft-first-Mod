8e4108389c6890053ff2d068e09b4c4aabc1858f
Andrew Fomin <andrfom1234567810@gmail.com>
2026-02-24 00:31:13 +0700

    Magic block added

--- src/main/java/net/lubcore/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/lubcore/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.lubcore.tutorialmod.block;
 
 import net.lubcore.tutorialmod.TutorialMod;
+import net.lubcore.tutorialmod.block.custom.MagicBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.ExperienceDroppingBlock;
@@ -26,6 +27,9 @@ public class ModBlocks {
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
                     AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(2f).requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
