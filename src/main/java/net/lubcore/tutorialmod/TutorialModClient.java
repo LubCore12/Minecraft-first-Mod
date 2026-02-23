231b899ee2580328b63068448c6795359b08a6e7
Andrew Fomin <andrfom1234567810@gmail.com>
2026-02-23 17:15:17 +0700

    Recipes, Drops and One special Item added

--- src/main/java/net/lubcore/tutorialmod/TutorialModClient.java
+++ src/main/java/net/lubcore/tutorialmod/TutorialModClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+         TutorialMod.LOGGER.info("Initializing client for " + TutorialMod.MOD_ID);
     }
 }
--- src/main/java/net/lubcore/tutorialmod/item/ModItems.java
+++ src/main/java/net/lubcore/tutorialmod/item/ModItems.java
@@ -1,9 +1,9 @@
 package net.lubcore.tutorialmod.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import jdk.jfr.Frequency;
 import net.lubcore.tutorialmod.TutorialMod;
+import net.lubcore.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,6 +14,9 @@ public class ModItems {
 
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
             new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel",
+            new ChiselItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
